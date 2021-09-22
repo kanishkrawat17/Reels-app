e2d79bf81173bb8f1297fb4c16674edce7139c7e
Kanishk <kanishkrawat67@gmail.com>
2021-09-22 11:08:39 +0530

    firestore add

--- src/App.js
+++ src/App.js
@@ -2,24 +2,48 @@ import Login from "./Login";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Authprovider from "./Authprovider";
+import {firestore} from "./firebase";
+import { useEffect } from "react";
 
 let App = () => {
+
+  useEffect(()=>{
+
+    firestore.collection("users").add({body : "this is val 1"})
+
+
+    async function f (){
+      let querySnapshot = await firestore.collection("users").get();
+      for(let i = 0 ; i < querySnapshot.docs.length ; i++){
+        console.log(querySnapshot.docs[i].data())
+      }
+    }
+    f();
+
+
+  },[]);
+
   return (
-    <Authprovider>
-      <Router>
-        <Switch>
 
-          <Route exact path="/login">
-            <Login />
-          </Route>
 
-          <Route exact path="/">
-            <Home />
-          </Route>
+
+    <h1>App</h1>
+    
+    // <Authprovider>
+    //   <Router>
+    //     <Switch>
+
+    //       <Route exact path="/login">
+    //         <Login />
+    //       </Route>
+
+    //       <Route exact path="/">
+    //         <Home />
+    //       </Route>
           
-        </Switch>
-      </Router>
-    </Authprovider>
+    //     </Switch>
+    //   </Router>
+    // </Authprovider>
   );
 };
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 // import firebase from "firebase/app";
 // import "firebase/auth";
 // import "firbase/firestore";
+import config from "./config.json"
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import config from "./config.json"
 
 
 firebase.initializeApp(config);//connected react app to firrbase
@@ -13,6 +13,7 @@ firebase.initializeApp(config);//connected react app to firrbase
 let provider = new firebase.auth.GoogleAuthProvider(); // we will use google services for login
 // console.log(provider);
 export const auth = firebase.auth(); // made auth to use firebase functions
+export const firestore = firebase.firestore();
 
 export const singInWithGoogle = ()=> {
   auth.signInWithPopup(provider); // we will use google services for login and with popup
